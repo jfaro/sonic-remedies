9cc2d3ae9ccd08211ff15db155383610df8fdb91
jfaro <jfaro@umich.edu>
2022-03-20 21:41:19 -0400

    Condense MusicForm form jsx

--- src/components/MusicForm.js
+++ src/components/MusicForm.js
@@ -12,13 +12,13 @@ export default function MusicForm() {
 
     const onFinish = (values) => {
         // Time added to database
-        let current = new Date(); 
-        let datetime = current.getFullYear() + '-' 
-                        + (current.getMonth() + 1) + '-' 
-                        + current.getDate() + ' ' 
-                        + current.getHours() + ":" 
-                        + current.getMinutes() + ":" 
-                        + current.getSeconds();
+        let current = new Date();
+        let datetime = current.getFullYear() + '-'
+            + (current.getMonth() + 1) + '-'
+            + current.getDate() + ' '
+            + current.getHours() + ":"
+            + current.getMinutes() + ":"
+            + current.getSeconds();
         values.timeAdded = datetime;
         // User adding to database
         values.admin = user.displayName;
@@ -27,7 +27,7 @@ export default function MusicForm() {
         values.length = fixedLength;
         let file = values.filename.split('.')[0];
 
-        set(ref(database, 'pieces/' + file), {values});
+        set(ref(database, 'pieces/' + file), { values });
 
         console.log("Success:", values);
     };
@@ -76,256 +76,224 @@ export default function MusicForm() {
     ]
 
     const keyOptions = [
-    {
-        value: 'a',
-        label: 'A',
-        children: modeOptions,
-    },
-    {
-        value: 'aSharp',
-        label: 'A♯',
-        children: modeOptions,
-    },
-    {
-        value: 'bFlat',
-        label: 'B♭',
-        children: modeOptions,
-    },
-    {
-        value: 'b',
-        label: 'B',
-        children: modeOptions,
-    },
-    {
-        value: 'c',
-        label: 'C',
-        children: modeOptions,
-    },
-    {
-        value: 'cSharp',
-        label: 'C♯',
-        children: modeOptions,
-    },
-    {
-        value: 'dFlat',
-        label: 'D♭',
-        children: modeOptions,
-    },
-    {
-        value: 'd',
-        label: 'D',
-        children: modeOptions,
-    },
-    {
-        value: 'dSharp',
-        label: 'D♯',
-        children: modeOptions,
-    },
-    {
-        value: 'eFlat',
-        label: 'E♭',
-        children: modeOptions,
-    },
-    {
-        value: 'e',
-        label: 'E',
-        children: modeOptions,
-    },
-    {
-        value: 'f',
-        label: 'F',
-        children: modeOptions,
-    },
-    {
-        value: 'fSharp',
-        label: 'F♯',
-        children: modeOptions,
-    },
-    {
-        value: 'gFlat',
-        label: 'G♭',
-        children: modeOptions,
-    },
-    {
-        value: 'g',
-        label: 'G',
-        children: modeOptions,
-    },
-    {
-        value: 'gSharp',
-        label: 'G♯',
-        children: modeOptions,
-    },
-    {
-        value: 'aFlat',
-        label: 'A♭',
-        children: modeOptions,
-    },
+        {
+            value: 'a',
+            label: 'A',
+            children: modeOptions,
+        },
+        {
+            value: 'aSharp',
+            label: 'A♯',
+            children: modeOptions,
+        },
+        {
+            value: 'bFlat',
+            label: 'B♭',
+            children: modeOptions,
+        },
+        {
+            value: 'b',
+            label: 'B',
+            children: modeOptions,
+        },
+        {
+            value: 'c',
+            label: 'C',
+            children: modeOptions,
+        },
+        {
+            value: 'cSharp',
+            label: 'C♯',
+            children: modeOptions,
+        },
+        {
+            value: 'dFlat',
+            label: 'D♭',
+            children: modeOptions,
+        },
+        {
+            value: 'd',
+            label: 'D',
+            children: modeOptions,
+        },
+        {
+            value: 'dSharp',
+            label: 'D♯',
+            children: modeOptions,
+        },
+        {
+            value: 'eFlat',
+            label: 'E♭',
+            children: modeOptions,
+        },
+        {
+            value: 'e',
+            label: 'E',
+            children: modeOptions,
+        },
+        {
+            value: 'f',
+            label: 'F',
+            children: modeOptions,
+        },
+        {
+            value: 'fSharp',
+            label: 'F♯',
+            children: modeOptions,
+        },
+        {
+            value: 'gFlat',
+            label: 'G♭',
+            children: modeOptions,
+        },
+        {
+            value: 'g',
+            label: 'G',
+            children: modeOptions,
+        },
+        {
+            value: 'gSharp',
+            label: 'G♯',
+            children: modeOptions,
+        },
+        {
+            value: 'aFlat',
+            label: 'A♭',
+            children: modeOptions,
+        },
     ];
 
     const { user } = useAuth();
 
     return (
         <Form
-        name="musicForm"
-        requiredMark={false}
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{
-            remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        style={{ width: '50%', maxWidth: '35rem' }}
-        >
-        <Form.Item
-            label="File Name"
-            name="filename"
-            rules={[
-            {
-                required: true,
-                message: "Please give a valid file name!",
-            },
-            ]}
-        >
-            {/* TODO: This needs to be changed to a custom Upload component */}
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="Track Title"
-            name="title"
-            rules={[
-            {
-                required: true,
-                message: "Please give the track's title!",
-            },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="Track Length"
-            name="length"
-            rules={[
-            {
-                required: true,
-                message: "Please give the track's length!",
-            },
-            ]}
-        >
-            <TimePicker format={'HH:mm'} showNow={false} onChange={onTimeChange}/>
-        </Form.Item>
-
-        <Form.Item
-            label="Artist Name"
-            name="artist"
-            rules={[
-            {
-                required: true,
-                message: "Please give the track's artist!",
-            },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="Album Title"
-            name="album"
-            rules={[
-            {
-                required: true,
-                message: "Please give the album's name!",
-            },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="Genre"
-            name="genre"
-            rules={[
-            {
-                required: true,
-                message: "Please give the track's genre!",
-            },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-            
-        <Form.Item
-            label="Track Tempo"
-            name="tempo"
-            rules={[
-            {
-                required: true,
-                message: "Please give the track's tempo!",
-            },
-            ]}
-        >
-            <InputNumber />
-        </Form.Item>
-
-        <Form.Item
-            label="Key and Mode"
-            name="key"
-            rules={[
-            {
-                required: true,
-                message: "Please give the track's key and mode!",
-            },
-            ]}
-        >
-            <Cascader options={keyOptions} placeholder="Please select" />
-        </Form.Item>
-
-        <Form.Item
-            label="Consistent Texture"
-            name="texture"
-            rules={[
-            {
-                required: true,
-                message: "Please indicate if the track has a consistent texture!",
-            },
-            ]}
-        >
-            <Radio.Group>
-                <Radio.Button value="y">Yes</Radio.Button>
-                <Radio.Button value="n">No</Radio.Button>
-            </Radio.Group>
-        </Form.Item>
-
-        <Form.Item
-            label="Improvisation"
-            name="improv"
-            rules={[
-            {
-                required: true,
-                message: "Please indicate if >50% of the song is improvised!",
-            },
-            ]}
-        >
-            <Radio.Group>
-                <Radio.Button value="y">Yes</Radio.Button>
-                <Radio.Button value="n">No</Radio.Button>
-            </Radio.Group>
-        </Form.Item>
-
-        <Form.Item
-            wrapperCol={{
-            offset: 8,
-            span: 16,
+            name="musicForm"
+            requiredMark={false}
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{
+                remember: true,
             }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            style={{ width: '50%', maxWidth: '35rem' }}
         >
-            <Button type="primary" htmlType="submit">
-            Submit
-            </Button>
-        </Form.Item>
+            <Form.Item
+                label="File Name"
+                name="filename"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please give a valid file name!",
+                    },
+                ]}
+            >
+                {/* TODO: This needs to be changed to a custom Upload component */}
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Track Title"
+                name="title"
+                rules={[{
+                    required: true,
+                    message: "Please give the track's title!",
+                }]}>
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Track Length"
+                name="length"
+                rules={[{
+                    required: true,
+                    message: "Please give the track's length!",
+                }]}>
+                <TimePicker format={'HH:mm'} showNow={false} onChange={onTimeChange} />
+            </Form.Item>
+
+            <Form.Item
+                label="Artist Name"
+                name="artist"
+                rules={[{
+                    required: true,
+                    message: "Please give the track's artist!",
+                }]}>
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Album Title"
+                name="album"
+                rules={[{
+                    required: true,
+                    message: "Please give the album's name!",
+                }]}>
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Genre"
+                name="genre"
+                rules={[{
+                    required: true,
+                    message: "Please give the track's genre!",
+                }]}>
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Track Tempo"
+                name="tempo"
+                rules={[{
+                    required: true,
+                    message: "Please give the track's tempo!",
+                }]}>
+                <InputNumber />
+            </Form.Item>
+
+            <Form.Item
+                label="Key and Mode"
+                name="key"
+                rules={[{
+                    required: true,
+                    message: "Please give the track's key and mode!",
+                }]}>
+                <Cascader options={keyOptions} placeholder="Please select" />
+            </Form.Item>
+
+            <Form.Item
+                label="Consistent Texture"
+                name="texture"
+                rules={[{
+                    required: true,
+                    message: "Please indicate if the track has a consistent texture!",
+                }]}>
+                <Radio.Group>
+                    <Radio.Button value="y">Yes</Radio.Button>
+                    <Radio.Button value="n">No</Radio.Button>
+                </Radio.Group>
+            </Form.Item>
+
+            <Form.Item
+                label="Improvisation"
+                name="improv"
+                rules={[{
+                    required: true,
+                    message: "Please indicate if >50% of the song is improvised!",
+                }]}>
+                <Radio.Group>
+                    <Radio.Button value="y">Yes</Radio.Button>
+                    <Radio.Button value="n">No</Radio.Button>
+                </Radio.Group>
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
         </Form>
     );
 }
