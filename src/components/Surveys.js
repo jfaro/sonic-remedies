564b593006cef9c17619d49d1487b7e92a5640c9
jfaro <jfaro@umich.edu>
2022-03-28 14:12:33 -0400

    Add status indicator to surveys section of admin page

--- src/components/Surveys.js
+++ src/components/Surveys.js
@@ -1,13 +1,19 @@
 import { db } from '../services/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
-import { Card, Col, Row, Space, Typography } from "antd";
+import { Card, Col, Row, Space, Spin, Tag, Typography } from "antd";
 const { Title } = Typography;
 
 const SurveyTile = ({ surveyData }) => {
-    const { title, questions, responses } = surveyData;
+    const { title, questions, responses, active } = surveyData;
+    const status = active ? <Tag color="green">Active</Tag> : <Tag color="red">Inactive</Tag>
+
     return (
-        <Card title={title} hoverable={true} style={{ height: '100%' }}>
+        <Card
+            title={title}
+            extra={status}
+            hoverable={true}
+            style={{ height: '100%' }}>
             <Space direction='vertical'>
                 <Title level={5}>Questions: {questions ? questions.length : 0}</Title>
                 <Title level={5}>Responses: {responses ? responses.length : 0}</Title>
@@ -19,6 +25,7 @@ const SurveyTile = ({ surveyData }) => {
 export default function Surveys() {
 
     const [surveys, setSurveys] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         fetchSurveys();
@@ -26,22 +33,27 @@ export default function Surveys() {
 
     async function fetchSurveys() {
         setSurveys([])
+        setLoading(true);
         const querySnapshot = await getDocs(collection(db, 'surveys'));
         querySnapshot.forEach(document => {
             const data = document.data();
             setSurveys(arr => [...arr, data]);
         })
+        setLoading(false);
     }
 
+    if (loading) return <Spin />
+
     return (
-        <Row gutter={24}>
+        < Row gutter={24} >
             {surveys.length > 0 ? surveys.map((survey, idx) => {
                 return (
                     <Col key={idx} xs={24} sm={12} md={8} lg={6} >
                         <SurveyTile surveyData={survey} key={idx} />
                     </Col>
                 )
-            }) : "No surveys found!"}
-        </Row>
+            }) : "No surveys found!"
+            }
+        </Row >
     )
 }
