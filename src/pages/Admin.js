861d417c0b38d4b462a41d0e212c53cef8a65b1c
Andrew Zhou <kiptoke@gmail.com>
2022-05-12 22:01:41 -0400

    Added admin menu

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,4 +1,6 @@
-import { Divider, Space, Typography, Row, Col } from "antd"
+import { useState } from 'react';
+import { Space, Typography, Menu } from "antd";
+import { PlayCircleOutlined, QuestionCircleOutlined, InfoCircleOutlined } from '@ant-design/icons';
 import MusicTable from "../components/MusicTable";
 import AllSurveys from '../components/survey/AllSurveys';
 import CreateSurvey from "../components/survey/CreateSurvey";
@@ -6,30 +8,81 @@ import CreateQuestionSet from "../components/survey/CreateQuestionSet";
 
 const { Title } = Typography;
 
+
 export default function Admin() {
+    const [current, setCurrent] = useState('tracks');
+
+    const items = [
+        {
+            label: 'Uploaded Tracks',
+            key: 'tracks',
+            icon: <PlayCircleOutlined/>,
+        },
+        {
+            label: 'Question Sets',
+            key: 'qsets',
+            icon: <QuestionCircleOutlined />,
+        },
+        {
+            label: 'Surveys',
+            key: 'surveys',
+            icon: <InfoCircleOutlined />,
+        }
+    ];
+
+    const onClick = (e) => {
+        setCurrent(e.key);
+    };
+
+    function QuestionSets() {
+        return(
+            <p>
+                <Title level={3}>Question Sets</Title>
+                <Space direction='horizontal' size='large'>
+                    <CreateQuestionSet />
+                </Space> 
+            </p>
+        );
+    }
+
+    function Surveys() {
+        return(
+            <p>
+                <Title level={3}>Surveys</Title>
+                <Space direction='vertical' size='large'>
+                    <CreateSurvey />
+                    <AllSurveys></AllSurveys>
+                </Space> 
+            </p>
+        );
+    }
+
+    function Tracks() {
+        return(
+            <p>
+                <Title level={3}>Uploaded Tracks</Title>
+                <MusicTable /> 
+            </p>
+        );
+    }
+
+    function AdminPage() {
+        if (current === 'tracks') {
+            return <Tracks></Tracks>
+        } else if ( current === 'qsets') {
+            return <QuestionSets></QuestionSets>
+        } else {
+            return <Surveys></Surveys>
+        }
+    }
 
     return (
         <div className="flex-col w-100">
-            <Row>
-                <Col span={12}>
-                    <Title level={3}>Question Sets</Title>
-                    <Space direction='vertical' size='large'>
-                        <CreateQuestionSet />
-                    </Space> 
-                </Col>
-                <Col span={12}>
-                    <Title level={3}>Surveys</Title>
-                    <Space direction='vertical' size='large'>
-                        <CreateSurvey />
-                    </Space> 
-                </Col>
-            </Row>
+            <p>
+                <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+            </p>
             
-
-            <Divider />
-
-            <Title level={3}>Uploaded Tracks</Title>
-            <MusicTable />
+            <AdminPage></AdminPage>
         </div>
     )
 }
